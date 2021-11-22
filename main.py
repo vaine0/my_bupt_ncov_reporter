280f90ff7b8e63bd3b0dae12828b6504ad43ab90
Vaine0 <58894729+vaine0@users.noreply.github.com>
2021-11-22 10:14:38 +0800

    未签到成功, 代码内循环, 不设置外循环

--- main.py
+++ main.py
@@ -61,18 +61,29 @@ def ncov_report(username, password, name, is_useold):
         raise RuntimeError('report_res 状态码不是 200')
     return post_data,report_res.text
 
-successs,ress,usernames,names,datas = [],[],[],[],[]
+successs,ress,usernames,names,datas,send_list = [],[],[],[],[],[]
 for user in  USERS:
-    success=True
-    username,password,name,useold=user
-    try:
-        data,res = ncov_report(username=username,password=password,name=name,is_useold=(useold==0))
-    except:
-        success = False
-        data,res = '',traceback.format_exc()
+    for i in range(3):
+        success=True
+        username,password,name,useold=user
+        try:
+            data,res = ncov_report(username=username,password=password,name=name,is_useold=(useold==0))
+        except:
+            success = False
+            data,res = '',traceback.format_exc()
 
-    print(f'{name}填报成功!服务器返回数据:\n{res}' if success else f'{name}填报失败!发生如下异常:\n{res}')
-    # print(f'填报数据:\n{data}\n')
+        print(f'{name}填报成功!服务器返回数据:\n{res}' if success else f'{name}填报失败!发生如下异常:\n{res}')
+        # print(f'填报数据:\n{data}\n')
+        try:
+            temp = eval(res)['m']
+            if temp=="操作成功":
+                send_list.append(name)
+                break
+            elif temp=="今天已经填报了":
+                break
+        except Exception as E:
+            url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id={CHAT_ID}&text={E}"
+            requests.get(url)
 
     successs+=[success]
     ress+=[res]
@@ -81,21 +92,22 @@ for user in  USERS:
     names+=[name]
 
 # send to telegram
-url = r"https://api.telegram.org/bot" + BOT_TOKEN + r"/sendMessage?chat_id=" + CHAT_ID + "&text="
-text = ""
-text += names[0] + '\n' + ress[0] + '\n\n'
-text += names[1] + '\n' + ress[1]
-url += text
-requests.get(url)
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id={CHAT_ID}&text="
+for send_name in send_list:
+    # text = "TEST\n"
+    text += send_name + '打卡成功'
+    url_send = url + text
+    requests.get(url_send)
 
-try:
-    notifier = ServerJiangNotifier(
-        sckey=SERVER_KEY,
-        sess=requests.Session()
-    )
-    print(f'通过「{notifier.PLATFORM_NAME}」给用户发送通知')
-    notifier.notify(success=successs, msg=ress,data=datas,username=usernames,name=names)
-except:
-    print(r"可能由于 「SERVER_KEY未设置」 或 「SERVER_KEY不正确」 或 「网络波动」 ，SERVER酱发送失败")
+
+# try:
+#     notifier = ServerJiangNotifier(
+#         sckey=SERVER_KEY,
+#         sess=requests.Session()
+#     )
+#     print(f'通过「{notifier.PLATFORM_NAME}」给用户发送通知')
+#     notifier.notify(success=successs, msg=ress,data=datas,username=usernames,name=names)
+# except:
+#     print(r"可能由于 「SERVER_KEY未设置」 或 「SERVER_KEY不正确」 或 「网络波动」 ，SERVER酱发送失败")
 
 
